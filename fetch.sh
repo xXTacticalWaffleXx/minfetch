569e41c540510eade7505aee380f54f84bab7379
Luna Aphelion <luna-aphelion@outlook.com>
2022-08-15 23:06:31 +0100

    dingus

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#a simple, configurable fetch
+
 #these varibles can be inserted into echo commands to make some text bold but not the rest
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -9,7 +11,8 @@ kernel=$(uname -r)
 uptime=$(uptime | awk -F'( |,|:)+' '{print $6,$7",",$8,"hours,",$9,"minutes."}')
 shell=$($SHELL --version)
 distro=$(head -n 1 /etc/os-release | cut -c 6- )
-
+#TODO: make this work with other package managers
+packages=$(pacman -Q | wc -l)
 #distro logos made with https://patorjk.com/software/taag/#p=display&f=Slant&t=
 
 if [[ $distro == "EndeavourOS" && $1 == "--ascii" ]]; then
@@ -25,6 +28,6 @@ echo -e "\e[35m$bold $USER\e[0m$normal@\e[35m$bold$hostname$normal"
 echo -e ""
 echo -e "\e[35m$bold kernel:    \e[0m$kernel"
 echo -e "\e[35m$bold uptime:    \e[0m$uptime"
-echo -e "\e[35m$bold packages:  \e[0m2,147,483,647 (pacman) 1 (snap)"
+echo -e "\e[35m$bold packages:  \e[0m$packages"
 echo -e "\e[35m$bold shell:     \e[0m$shell"
 echo -e "\e[35m$bold distro:    \e[0m$distro"
