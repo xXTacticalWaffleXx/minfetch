21caa4b7692d58e659ab15e745c8c2655f10454a
Luna Aphelion <luna-aphelion@proton.me>
2022-08-17 18:22:47 +0100

    added a flag to show the unix timestamp

--- fetch.sh
+++ fetch.sh
@@ -4,11 +4,12 @@
 
 #===================PARSES CLI SWITCHES
 
-while getopts "apd" o; do
+while getopts "apdu" o; do
     case $o in
         a) ascii_flag=true ;;
         p) pkgmeme_flag=true ;;
         d) debug=true ;; # this is a debug flag used for different things at different times, do not use this outside of development, its effects will change
+        u) show_unix_time=true ;;
     esac
 done
 
@@ -26,7 +27,7 @@ uptime=$(echo $(uptime -p | sed 's/up//' | sed 's/ute//'))
 shell_raw=$($SHELL --version)
 shell=$(echo $shell_raw | sed 's/h.*/h/')
 distro=$(head -n 1 /etc/os-release | cut -c 6- )
-
+unix_timestamp=$(date '+%s')
 #todo: add portage and dnf support
 
 pacman -Q > /dev/null 2>&1 #just runs pacman to check if its installed
@@ -73,6 +74,9 @@ echo -e "\e[35m$bold uptime:    \e[0m$uptime"
 echo -e "\e[35m$bold packages:  \e[0m$packages"
 echo -e "\e[35m$bold shell:     \e[0m$shell"
 echo -e "\e[35m$bold distro:    \e[0m$distro"
+if [[ $show_unix_time == "true" ]]; then 
+    echo -e "\e[35m$bold unix time: \e[0m$unix_timestamp" # this is in an if statement because it is a meme that most people arent gonna use
+fi
 echo -e ""
 echo -e "\e[1;30m ██\e[1;31m ██\e[1;32m ██\e[1;33m ██\e[1;34m ██\e[1;35m ██\e[1;36m ██\e[1;37m ██"
 echo -e ""
