43c1574b58b0040ed61593663ebbf29835dc5c9e
Luna Aphelion <luna-aphelion@proton.me>
2022-08-21 06:54:24 +0100

    added gpl disclaimer

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+#copyright luna aphelion <luna-aphelion@proton.me>
 
-#a simple, configurable fetch
+# this is a very simple script that just runs a single g++ command, its
+# just here to save the user the trouble of typing the whole command for
+# each update, i found it useful and hope you will too
+
+#Copyright(C) 2022 luna aphelion
+#
+#This program is free software : you can redistribute itand /or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.If not, see < https://www.gnu.org/licenses/>.
+
 
 #PARSES CLI SWITCHES i need to figure out how to get this at the bottom
 
